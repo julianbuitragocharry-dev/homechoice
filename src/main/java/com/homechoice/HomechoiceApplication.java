544a94e53d825cdcfd65cc9239c082fecfdfeaae
Julian Enrique <julian.buitrago3003@gmail.com>
2024-10-06 22:09:44 -0500

    initial commit

---
+++ src/main/java/com/homechoice/HomechoiceApplication.java
@@ -0,0 +1,13 @@
+package com.homechoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomechoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomechoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homechoice/HomechoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homechoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomechoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
