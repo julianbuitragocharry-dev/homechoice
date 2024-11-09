bb978de0010e43a10dc9f3e85f351b906bd453d7
Julian Enrique <julian.buitrago3003@gmail.com>
2024-11-09 12:37:24 -0500

    feat: add filters user endpoints

--- src/main/java/com/homechoice/repositories/users/UserRepository.java
+++ src/main/java/com/homechoice/repositories/users/UserRepository.java
@@ -1,14 +1,33 @@
 package com.homechoice.repositories.users;
 
 import com.homechoice.entities.users.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
-    List<User> findByRoles_Rol(String roleName);
+
+    @Query("SELECT u FROM User u " +
+            "WHERE (u.nit IS NULL OR u.nit ILIKE :nit)")
+    Page<User> findAll(
+            @Param("nit") String nit,
+            Pageable pageable
+    );
+
+    @Query("SELECT u FROM User u " +
+            "JOIN u.roles r " +
+            "WHERE (r.rol = :roleName)" +
+            "AND (u.nit IS NULL OR u.nit ILIKE :nit)")
+    Page<User> findByRolesRol(
+            String roleName,
+            @Param("nit") String nit,
+            Pageable pageable
+    );
 }
