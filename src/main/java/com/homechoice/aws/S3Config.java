802d4002a8183d17b0c4e6efa8a77901339b664f
Julian Enrique <julian.buitrago3003@gmail.com>
2024-11-10 21:51:16 -0500

    refactor aws layer and application.yaml
    add docs and injected attributes

--- src/main/java/com/homechoice/aws/S3Config.java
+++ src/main/java/com/homechoice/aws/S3Config.java
@@ -8,24 +8,47 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+/**
+ * Configuration class for AWS S3 client.
+ * Sets up an S3Client bean with the necessary credentials and region.
+ */
 @Configuration
 public class S3Config {
-    @Value("${spring.cloud.aws.credentials.access-key}")
+    /**
+     * AWS Access Key for authentication.
+     */
+    @Value("${cloud-storage.s3.access-key}")
     private String accessKey;
 
-    @Value("${spring.cloud.aws.credentials.secret-key}")
+    /**
+     * AWS Access Key for authentication.
+     */
+    @Value("${cloud-storage.s3.secret-key}")
     private String secretKey;
 
-    @Value("${spring.cloud.aws.region.static}")
+    /**
+     * AWS region for the S3 client.
+     */
+    @Value("${cloud-storage.s3.region}")
     private String region;
 
+    /**
+     * Creates and configures an S3Client bean.
+     * @return Configured {@link S3Client} instance.
+     */
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials()))
                 .build();
+    }
+
+    /**
+     * Builds AWS credentials using access and secret keys.
+     * @return Configured {@link AwsBasicCredentials} instance with configured access and secret keys.
+     */
+    private AwsBasicCredentials awsCredentials() {
+        return AwsBasicCredentials.create(accessKey, secretKey);
     }
 }
--- src/main/java/com/homechoice/aws/S3Service.java
+++ src/main/java/com/homechoice/aws/S3Service.java
@@ -1,6 +1,8 @@
 package com.homechoice.aws;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -12,44 +14,74 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Service for handling file operations with AWS S3.
+ */
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class S3Service {
     private final S3Client s3Client;
 
+    /**
+     * AWS S3 Bucket name configured in application properties.
+     */
+    @Value("${cloud-storage.s3.bucket-name}")
+    private String bucketName;
+
+    /**
+     * Uploads a single file to the configured S3 bucket.
+     *
+     * @param file the {@link MultipartFile} to be uploaded.
+     * @return the URL of the uploaded file.
+     * @throws IOException if an error occurs while uploading the file.
+     */
     public String uploadFile(MultipartFile file) throws IOException {
         try {
             String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket("homechoicebucket")
+                    .bucket(bucketName)
                     .key(fileName)
                     .build();
 
             s3Client.putObject(putObjectRequest,
                     software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes()));
 
-            return "https://homechoicebucket.s3.us-east-2.amazonaws.com/" + fileName.replace(" ", "%20");
+            return "https://" + bucketName + ".s3.us-east-2.amazonaws.com/" + fileName.replace(" ", "%20");
         } catch (Exception e) {
-            throw new IOException(e);
+            throw new IOException("Error uploading file: " + e.getMessage());
         }
     }
 
+    /**
+     * Deletes a file from the configured S3 bucket based on the file URL.
+     *
+     * @param path the URL of the file to be deleted.
+     * @throws IOException if an error occurs while deleting the file.
+     */
     public void deleteFile(String path) throws IOException {
         try {
             String fileName = path.replace("https://homechoicebucket.s3.us-east-2.amazonaws.com/", "");
             String key = fileName.replace("%20", " ");
 
             DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
-                    .bucket("homechoicebucket")
+                    .bucket(bucketName)
                     .key(key)
                     .build();
 
             s3Client.deleteObject(deleteObjectRequest);
         } catch (Exception e) {
-            throw new IOException("Error deleting the file: " + e.getMessage(), e);
+            throw new IOException("Error deleting file: " + e.getMessage(), e);
         }
     }
 
+    /**
+     * Uploads multiple files to the configured S3 bucket.
+     *
+     * @param files a list of {@link MultipartFile} objects to be uploaded.
+     * @return a list of URLs of the uploaded files.
+     * @throws IOException if an error occurs while uploading any of the files.
+     */
     public List<String> uploadFiles(List<MultipartFile> files) throws IOException {
         List<String> uploadPaths = new ArrayList<>();
         for (MultipartFile file : files) {
@@ -59,6 +91,12 @@ public class S3Service {
         return uploadPaths;
     }
 
+    /**
+     * Deletes multiple files from the configured S3 bucket.
+     *
+     * @param paths a list of URLs of the files to be deleted.
+     * @throws IOException if an error occurs while deleting any of the files.
+     */
     public void deleteFiles(List<String> paths) throws IOException {
         for (String path : paths) {
             deleteFile(path);
