03669138c680c9ecc70866356b413b93edaa303c
Julian Enrique <julian.buitrago3003@gmail.com>
2024-11-14 14:37:17 -0500

    fix: rename @RestControllerAdvice
    >:(

--- src/main/java/com/homechoice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/homechoice/exception/GlobalExceptionHandler.java
@@ -3,38 +3,42 @@ package com.homechoice.exception;
 import com.homechoice.dto.MessageResponse;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public MessageResponse handleGlobalException(Exception e) {
-        return new MessageResponse(
+    public ResponseEntity<MessageResponse> handleGlobalException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            .body(new MessageResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 e.getMessage(),
-                LocalDateTime.now()
-        );
+                LocalDateTime.now())
+            );
     }
 
     @ExceptionHandler(BadCredentialsException.class)
-    public MessageResponse handleBadCredentialsException(BadCredentialsException e) {
-        return new MessageResponse(
+    public ResponseEntity<MessageResponse> handleBadCredentialsException(BadCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED.value())
+            .body(new MessageResponse(
                 HttpStatus.UNAUTHORIZED.value(),
                 e.getMessage(),
-                LocalDateTime.now()
-        );
+                LocalDateTime.now())
+            );
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public MessageResponse handleEntityNotFoundException(EntityNotFoundException e) {
-        return new MessageResponse(
+    public ResponseEntity<MessageResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value())
+            .body(new MessageResponse(
                 HttpStatus.NOT_FOUND.value(),
                 e.getMessage(),
-                LocalDateTime.now()
-        );
+                LocalDateTime.now())
+            );
     }
 }
